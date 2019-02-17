eaa1e946653b4ec489d76d2ae5c10b3f721c1d2c
D. Scott Boggs <scott@tams.tech>
2019-02-17 17:08:55 -0500

    DRYer specs

--- spec/detect_spec.cr
+++ spec/detect_spec.cr
@@ -1,21 +1,13 @@
 require "./spec_helper"
 
-{% for filetype in READY %}
-describe Subtitles::{{filetype.id}} do
-  it "is detected" do
-    Subtitles::{{filetype.id}}
-      .detect(fixture "{{filetype.id.downcase}}")
-      .should eq Subtitles::{{filetype.id}}
+{% for status in [READY, PENDING] %}
+\{% for filetype in {{status.id}} %}
+describe Subtitles::\{{filetype.id}} do
+  \{% if {{status}} == READY %}it\{% else %}pending\{% end %} "is detected" do
+    Subtitles::\{{filetype.id}}
+      .detect(fixture "\{{filetype.id.downcase}}")
+      .should eq Subtitles::\{{filetype.id}}
   end
 end
-{% end %}
-
-{% for filetype in PENDING %}
-describe "Subtitles::{{filetype.id}}" do
-  pending "is detected" do
-    Subtitles::{{filetype.id}}
-      .detect(fixture "{{filetype.id.downcase}}")
-      .should eq Subtitles::{{filetype.id}}
-  end
-end
+\{% end %}
 {% end %}
--- spec/parse_spec.cr
+++ spec/parse_spec.cr
@@ -1,29 +1,17 @@
 require "./spec_helper"
 
-{% for filetype in READY %}
-describe Subtitles::{{filetype.id}} do
-  it "parses the example text" do
-    content = fixture "{{filetype.id.downcase}}"
-    captions = Subtitles::{{filetype.id}}.new(content).parse eol: "\n"
+{% for status in [READY, PENDING] %}
+\{% for filetype in {{status}} %}
+describe Subtitles::\{{filetype.id}} do
+  \{% if {{status}} == READY %}it\{% else %}pending\{% end %} "parses the example text" do
+    content = fixture "\{{filetype.id.downcase}}"
+    captions = Subtitles::\{{filetype.id}}.new(content).to_captions eol: "\n"
     captions.size.should be > 1
     # TODO better tests of parsed data
     if File.directory? "./output"
-      File.write(filename: "./output/parsed.{{filetype.id}}.json", content: captions.to_s)
+      File.write(filename: "./output/parsed.\{{filetype.id}}.json", content: captions.to_s)
     end
   end
 end
-{% end %}
-
-{% for filetype in PENDING %}
-describe "Subtitles::{{filetype.id}}" do
-  pending "parses the example text" do
-    content = fixture "{{filetype.id.downcase}}"
-    captions = Subtitles::{{filetype.id}}.new(content).parse eol: "\n"
-    captions.size.should be > 1
-    # TODO better tests of parsed data
-    if File.directory? "./output"
-      File.write(filename: "./output/parsed.{{filetype.id}}.json", content: captions.to_s)
-    end
-  end
-end
+\{% end %}
 {% end %}
